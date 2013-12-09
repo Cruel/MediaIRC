02f5a2edade83a4cf3174cc3047107aea8d4e56a
Thomas Edvalson <machin3@gmail.com>
2013-12-08 20:41:08 -0500

    Update "cake" console script to use composer's CakePHP installation

--- Console/cake.php
+++ Console/cake.php
@@ -27,7 +27,7 @@ if (function_exists('ini_set')) {
 
 	// the following line differs from its sibling
 	// /app/Console/cake.php
-	ini_set('include_path', $root . PATH_SEPARATOR .  $ds . 'media' . $ds . 'hdd' . $ds . 'thomas' . $ds . 'dev' . $ds . 'cakephp' . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+	ini_set('include_path', $root . PATH_SEPARATOR . dirname(dirname(__FILE__)). $ds . 'Vendor' . $ds . 'pear-pear.cakephp.org' . $ds . 'CakePHP' . PATH_SEPARATOR . ini_get('include_path'));
 }
 
 if (!include ($dispatcher)) {
